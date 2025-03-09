8ab69857b11457846a40033b8f14e1a3bb15c42b
Ishan Bhat <ishan.s.bhat@gmail.com>
2025-03-09 16:18:49 +0530

    Review_service
    This contains the code for the review microservice

--- back-end/sellerService/src/main/java/com/mohil_bansal/sellerservice/seller_service/exception/DataAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.mohil_bansal.sellerservice.seller_service.exception;
-
-public class DataAlreadyExistsException extends RuntimeException{
-
-    public  DataAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- back-end/sellerService/src/main/java/com/mohil_bansal/sellerservice/seller_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mohil_bansal.sellerservice.seller_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
